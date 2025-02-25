19fcb317234880f60b2135b78f0852ec2d97bd72
Sakira Maharjan <maharjansaki@gmail.com>
2025-02-25 13:58:44 +0545

    Array

---
+++ Array/numberMethods.js
@@ -0,0 +1,17 @@
+//isNaN()
+console.log(isNaN("Hello"));   
+console.log(isNaN(123));       
+console.log(isNaN("123"));     
+console.log(isNaN(NaN));       
+
+//isFinite()
+console.log(isFinite(3/0));  
+
+//isInterger()
+console.log(Number.isInteger(5.5));  
+
+//parseFloat()
+console.log(parseFloat("3.14"));  
+
+//parseFloat()
+console.log(parseInt("3.10")); 
